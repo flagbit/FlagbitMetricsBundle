9c5038882100edd226e072242eeb74377a7f36f7
Antonio Mansilla <antonio.mansilla@flagbit.de>
2014-10-22 17:36:47 +0200

    Removed unused variable

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('flagbit_metrics');
+        $treeBuilder->root('flagbit_metrics');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
